dd69d9b534a2b358e678ab8bf9519231603c8e09
Omer Erdem <m.o.erdem@outlook.com>
2020-09-27 16:11:19 +0200

    #0 Create project

---
+++ app/src/main/java/nl/omererdem/madlevel4task2/MainActivity.kt
@@ -0,0 +1,11 @@
+package nl.omererdem.madlevel4task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
