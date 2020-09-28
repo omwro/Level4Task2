513b881ff5da7777444ec5d60b65c15d3db3398c
Omer Erdem <m.o.erdem@outlook.com>
2020-09-28 10:48:42 +0200

    #2.2 Room integrations

---
+++ app/src/main/java/nl/omererdem/madlevel4task2/GameDao.kt
@@ -0,0 +1,18 @@
+package nl.omererdem.madlevel4task2
+
+import androidx.room.*
+
+@Dao
+interface GameDao {
+    @Query("SELECT * FROM gameTable")
+    suspend fun getAllGames(): List<Game>
+
+    @Insert
+    suspend fun insertGame(game: Game)
+
+    @Delete
+    suspend fun deleteGame(game: Game)
+
+    @Update
+    suspend fun updateGame(game: Game)
+}
---
+++ app/src/main/java/nl/omererdem/madlevel4task2/GameRepository.kt
@@ -0,0 +1,28 @@
+package nl.omererdem.madlevel4task2
+
+import android.content.Context
+
+class GameRepository(context: Context) {
+    private val gameDao: GameDao
+
+    init {
+        val gameRoomDatabase = GameRoomDatabase.getDatabase(context)
+        gameDao = gameRoomDatabase!!.gameDao()
+    }
+
+    suspend fun getAllGames(): List<Game> {
+        return gameDao.getAllGames()
+    }
+
+    suspend fun insertGame(game: Game) {
+        gameDao.insertGame(game)
+    }
+
+    suspend fun deleteGame(game: Game) {
+        gameDao.deleteGame(game)
+    }
+
+    suspend fun updateGame(game: Game) {
+        gameDao.updateGame(game)
+    }
+}
