baa2f9dfe5e506021ad5bbf7a354c61e5294ebc3
Omer Erdem <m.o.erdem@outlook.com>
2020-09-28 10:54:21 +0200

    #2.3 Init nav

--- app/src/main/java/nl/omererdem/madlevel4task2/GameFragment.kt
+++ app/src/main/java/nl/omererdem/madlevel4task2/GameFragment.kt
@@ -1,6 +1,26 @@
 package nl.omererdem.madlevel4task2
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class GameFragment: Fragment() {
+class GameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+
+    }
 }
