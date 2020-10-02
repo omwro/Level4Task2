b5928881e28b750927e5f18494ad89bb68ced2b6
Omer Erdem <m.o.erdem@outlook.com>
2020-10-02 20:00:45 +0200

    #2.5 New enum models

---
+++ app/src/main/java/nl/omererdem/madlevel4task2/model/Result.kt
@@ -0,0 +1,30 @@
+package nl.omererdem.madlevel4task2.model
+
+enum class Result() {
+    WON {
+        override fun getString(): String = "You won!"
+        override fun getId(): Int = 1
+    },
+    DRAW {
+        override fun getString(): String = "A draw!"
+        override fun getId(): Int = 0
+    },
+    LOST {
+        override fun getString(): String = "You Lost!"
+        override fun getId(): Int = -1
+    };
+
+    abstract fun getString(): String
+    abstract fun getId(): Int
+
+    companion object {
+        fun findResult(id: Int): Result? {
+            for (result in values()) {
+                if (result.getId() == id) {
+                    return result
+                }
+            }
+            return null
+        }
+    }
+}
